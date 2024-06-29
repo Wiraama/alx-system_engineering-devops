a84d129dbe8f260cc8c026cff913924777f413a7
Wiraama <mawira01@students.uonbi.ac.ke>
2024-06-28 18:04:31 -0700

    1-install_a_package.pp

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+# installing flast from pip3
+
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['Python3-pip'],
+}
