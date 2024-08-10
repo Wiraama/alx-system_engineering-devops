32c65418448a2560d3095bb756f5491d50bbbbe2
Wiraama <mawira01@students.uonbi.ac.ke>
2024-08-10 10:02:10 +0300

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,45 +1,27 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to CSV format."""
-import csv
-import requests
-import sys
-
-
-def main():
-    if len(sys.argv) < 2:
-        print("Usage: {} <user_id>".format(sys.argv[0]))
-        sys.exit(1)
-
-        user_id = sys.argv[1]
-        url = "https://jsonplaceholder.typicode.com/"
-
-        try:
-            user = requests.get(url + "users/{}".format(user_id)).json()
-            username = user.get("username")
-            todos = requests.get(
-                    url + "todos",
-                    params={"userId": user_id}
-                    ).json()
-
-            print("User ID:", user_id)
-            print("Username:", username)
-            print("Number of tasks:", len(todos))
-
-            with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-                writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-                for t in todos:
-                    writer.writerow([
-                        user_id,
-                        username,
-                        t.get("completed"),
-                        t.get("title")
-                        ])
-
-        except requests.exceptions.RequestException as e:
-            print("Error fetching data:", e)
-        except Exception as e:
-            print("An error occurred:", e)
+"""
+place holder
+"""
 
 
 if __name__ == "__main__":
-    main()
+
+    import csv
+    import requests
+    from sys import argv
+
+    if len(argv) < 2:
+        exit()
+    todos = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}"
+        .format(argv[1]))
+    name = requests.get(
+        "https://jsonplaceholder.typicode.com/users?id={}".format(argv[1]))
+    name = name.json()
+    name = name[0]["username"]
+    todos = todos.json()
+    file_name = "{}.csv".format(argv[1])
+    with open(file_name, 'w') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',', quoting=csv.QUOTE_ALL)
+        for todo in todos:
+            writer.writerow([argv[1], name, todo['completed'], todo['title']])
