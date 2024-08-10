cf700dee5afb0fb3038505e62b36d8974e20dcf9
Wiraama <mawira01@students.uonbi.ac.ke>
2024-08-10 10:06:16 +0300

    3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-"""Exports to-do list information of all employees to JSON format."""
-import json
-import requests
+"""
+place holder
+"""
 
 
 if __name__ == "__main__":
-        url = "https://jsonplaceholder.typicode.com/"
-            users = requests.get(url + "users").json()
-
-                with open("todo_all_employees.json", "w") as jsonfile:
-                            json.dump({
-                                            u.get("id"): [{
-                                                                "task": t.get("title"),
-                                                                                "completed": t.get("completed"),
-                                                                                                "username": u.get("username")
-                                                                                                            } for t in requests.get(url + "todos",
-                                                                                                                                                    params={"userId": u.get("id")}).json()]
-                                                                                                                        for u in users}, jsonfile)
+    import requests
+    import json
+    users = requests.get(
+        "https://jsonplaceholder.typicode.com/users")
+    users = users.json()
+    result = {}
+    for user in users:
+        todos = requests.get(
+            "https://jsonplaceholder.typicode.com/todos?userId={}"
+            .format(user["id"]))
+        todos = todos.json()
+        result[user["id"]] = []
+        for todo in todos:
+            result[user["id"]].append(
+                {"username": user["username"],
+                    "task": todo["title"], "completed": todo["completed"]})
+    with open("todo_all_employees.json", 'w') as result_file:
+        json.dump(result, result_file)
