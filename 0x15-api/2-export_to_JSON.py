1080a6a1f2886470518fc2c460e7c188c62feb07
mawira <mawira01@students.uonbi.ac.ke>
2024-08-10 09:32:13 +0300

    mawira

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -11,11 +11,14 @@ if __name__ == "__main__":
 
         user_id = sys.argv[1]
         url = "https://jsonplaceholder.typicode.com/"
-            
+
             try:
                 user = requests.get(url + "users/{}".format(user_id)).json()
                 username = user.get("username")
-                todos = requests.get(url + "todos", params={"userId": user_id}).json()
+                todos = requests.get(i
+                        url + "todos",
+                        params={"userId": user_id}
+                        ).json()
 
                 with open("{}.json".format(user_id), "w") as jsonfile:
                     json.dump(
